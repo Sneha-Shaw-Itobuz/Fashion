0887c2ad40c4443bef0e95ab13f0b44d1487ab2c
Arpana <arpana@itobuz.com>
2023-03-03 16:19:34 +0530

    created-nav

---
+++ js/script.js
@@ -0,0 +1,13 @@
+(()=>{
+   new Swiper(".follow-us-swipper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        grabCursor: true,
+        // loop:true,
+        autoplay: {
+            delay:1000,
+            disableOnInteraction:false
+        }
+      });
+})()
